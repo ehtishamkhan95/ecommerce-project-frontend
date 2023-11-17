430c5c3841dace1da390d772dab2e4667cc41912
Ehtisham Khan <109791437+ehtishamkhan95@users.noreply.github.com>
2023-11-17 11:08:01 +0500

    Update apiService.js

--- src/utils/apiService.js
+++ src/utils/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiService = axios.create({
-    baseURL: "http://localhost:3000/api/",
+    baseURL: "https://nice-lime-kangaroo-yoke.cyclic.app/api/",
     withCredentials: true
 })
 
